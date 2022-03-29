4a7dfccf99047732bca6ab632b7affd257a0212d
luizsmatos <gusstavopdm@gmail.com>
2022-03-29 15:50:29 -0300

    feat: adicionando GlobalStyles

---
+++ src/styles/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+  :root {
+    --violet-red: #B22E6F;
+    --white: #FFFFFF;
+    --gray-20: #333333;
+
+    @media (max-width: 1080px) {
+      html {
+        font-size: 93.75%;
+      }
+    }
+
+    @media (max-width: 720px) {
+      html {
+        font-size: 87.5%;
+      }
+    }
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body, input, button {
+    font: 14px Heebo, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyles;
