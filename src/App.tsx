6527936dbe67ce7f2cea0f0532e98fb96e734381
luizsmatos <gusstavopdm@gmail.com>
2022-03-31 09:57:41 -0300

    feature: adicionando ToastContainer, App

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,26 @@ import Routes from 'routes';
 import { AuthProvider } from 'hooks/useAuth';
 
 import GlobalStyles from 'styles/global';
+import 'react-toastify/dist/ReactToastify.min.css';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <AuthProvider>
       <GlobalStyles />
       <Routes />
+
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </AuthProvider>
   );
 };
